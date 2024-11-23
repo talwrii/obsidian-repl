25a2bc1a2c729a504a63c22541305443244567e0
Tal Wrii <talwrii@gmail.com>
2024-11-24 00:54:15 +0100

    Create repl plugin

--- main.ts
+++ main.ts
@@ -1,134 +1,200 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import * as util from 'util'
+
+import * as evalScope from "./evalScope"
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+    mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+    mySetting: 'default'
+}
+
+
+class Scope {
+    // https://stackoverflow.com/questions/2051678/getting-all-variables-in-scope
+    context: Object
+
+    constructor() {
+        this.context = {};
+    }
+
+    add(name: string, x: any) {
+        this.context[name] = x
+    }
+
+    eval(s: string) {
+        console.log("scope s:" + s)
+        return evalScope.evalInScope(s, this.context)
+    }
+}
+
+function dir(obj: any) {
+    var p = [];
+    for (; obj != null; obj = Object.getPrototypeOf(obj)) {
+        var op = Object.getOwnPropertyNames(obj);
+        for (var i = 0; i < op.length; i++)
+            if (p.indexOf(op[i]) == -1)
+                p.push(op[i]);
+    }
+    return p;
+}
+
+const formatObj = (x: any) => {
+    console.log("formatting")
+    if (x === undefined) {
+        return "undefined";
+    } else if (typeof x === "string") {
+        return x.toString()
+    } else if (typeof x === "boolean") {
+        return x.toString()
+    } else if (typeof x === "number") {
+        return x.toString()
+    } else if (x.constructor === undefined) {
+        return util.inspect(x)
+    } else if (x.constructor === Array) {
+        return x.toString()
+    } else if (x.constructor === Object) {
+        return util.inspect(x)
+    } else {
+        console.log(x.constructor.name)
+        return `[${x.constructor.name} object]`
+    }
+}
+
+const makeEndOfLine = (editor: any) => {
+    return () => {
+        const cursor = editor.getCursor()
+        const line = editor.getLine(cursor.line)
+        editor.setCursor(cursor.line, line.length)
+    }
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+    settings: MyPluginSettings;
+    scope: Scope
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        this.scope = new Scope()
+        await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        this.addCommand({
+            id: 'repl-execute',
+            name: 'Execute the current line',
+            editorCallback: (editor: Editor, view: MarkdownView) => {
+                const cursor = editor.getCursor()
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+                const region = editor.getSelection() || editor.getLine(cursor.line)
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+                let output: string = "";
+                const endOfLine = makeEndOfLine(editor)
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+                this.scope.add("plugin", this)
+                this.scope.add("app", this.app)
+                this.scope.add("editor", editor)
+                this.scope.add("view", view)
+                this.scope.add("endOfLine", endOfLine)
+                this.scope.add("dir", dir)
+                try {
+                    output = formatObj(this.scope.eval(region))
+                } catch (e) {
+                    new Notice(e.toString())
+                    return;
+                }
+                editor.setCursor(cursor.line + 1, 0)
+                editor.replaceRange(output + "\n", editor.getCursor())
+                endOfLine()
+            }
+        });
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+        // This adds a complex command that can check whether the current state of the app allows execution of the command
+        this.addCommand({
+            id: 'open-sample-modal-complex',
+            name: 'Open sample modal (complex)',
+            checkCallback: (checking: boolean) => {
+                // Conditions to check
+                const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+                if (markdownView) {
+                    // If checking is true, we're simply "checking" if the command can be run.
+                    // If checking is false, then we want to actually perform the operation.
+                    if (!checking) {
+                        new SampleModal(this.app).open();
+                    }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+                    // This command will only show up in Command Palette when the check function returns true
+                    return true;
+                }
+            }
+        });
 
-	onunload() {
+        // This adds a settings tab so the user can configure various aspects of the plugin
+        this.addSettingTab(new SampleSettingTab(this.app, this));
 
-	}
+        // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+        // Using this function will automatically remove the event listener when this plugin is disabled.
+        this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+            console.log('click', evt);
+        });
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+        // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+        this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+    }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    onunload() {
+
+    }
+
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
 
 class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+    constructor(app: App) {
+        super(app);
+    }
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    onOpen() {
+        const { contentEl } = this;
+        contentEl.setText('Woah!');
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+    onClose() {
+        const { contentEl } = this;
+        contentEl.empty();
+    }
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+    plugin: MyPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    constructor(app: App, plugin: MyPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	display(): void {
-		const {containerEl} = this;
+    display(): void {
+        const { containerEl } = this;
 
-		containerEl.empty();
+        containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Setting(containerEl)
+            .setName('Setting #1')
+            .setDesc('It\'s a secret')
+            .addText(text => text
+                .setPlaceholder('Enter your secret')
+                .setValue(this.plugin.settings.mySetting)
+                .onChange(async (value) => {
+                    this.plugin.settings.mySetting = value;
+                    await this.plugin.saveSettings();
+                }));
+    }
 }
